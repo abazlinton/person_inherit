42369ad09fade7a8a33b7b2c7ede3ba670c310c1
Alex Bazlinton <abazlinton@gmail.com>
2016-10-09 21:45:52 +0100

    finally

--- specs/medic_spec.rb
+++ specs/medic_spec.rb
@@ -8,17 +8,10 @@ class MedicTest < MiniTest::Test
 		@medic = Medic.new({first_name: "Alex", last_name:"Bazlinton"})
 	end
 
-	def test_medic_can_say_full_name
-		assert_equal("Alex Bazlinton", @medic.greeting)
-
-	end
 
 	def test_medic_can_heal
 		assert_equal("HEAL!!!", @medic.heal)
 
 	end
-
-
-
 
 end
